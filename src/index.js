75f8028d3d67d48e19ea2624c29892c02005ad5f
Berkolds <deivids.berkolds@accenture.com>
2020-03-22 16:28:26 +0200

    backup commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore({
+  courses: [
+    { key: 1, title: "test" },
+    { key: 2, title: "test1" }
+  ]
+});
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
